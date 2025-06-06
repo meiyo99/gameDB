a71f9a2d075b3fdd93228656e5d5ce9b903b2b4a
meiyo99 <mayureshnaiduu@gmail.com>
2025-06-06 12:24:46 -0400

    added project folder and NuGet packages

---
+++ GameDB/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GameDB.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
